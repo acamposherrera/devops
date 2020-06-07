6210397047e93680476f069b7a9ce30a3effb489
Andrés Campos <andres.campos@spbox.cl>
2020-06-07 16:41:00 -0400

    AB#9 Comentario de VS2019

--- ArquitecturaBase/ArquitecturaBase/WeatherForecast.cs
+++ ArquitecturaBase/ArquitecturaBase/WeatherForecast.cs
@@ -12,6 +12,7 @@ namespace ArquitecturaBase
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
+        // Nuevo comentario VS2019
         public string Summary { get; set; }
     }
 }
